4070fa6f1f5cc2aa73670df2eb4833835827c22a
Conor Schofield <conorschofield@lbl.gov>
2024-06-14 15:03:24 -0400

    Initial Maven Setup and basic implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# phoebus-olog-email-notifier-module
+# Phoebus Olog Email Notifier Module
+
+This is an optional module for the Phoebus Olog electronic logbook service, see https://github.com/Olog/phoebus-olog. Its purpose is to send an email when a log is sent based on a list of emails mapped to the logbook entry's tag.
---
+++ src/test/java/org/phoebus/olog/email/EmailNotifierTest.java
@@ -0,0 +1,20 @@
+package org.phoebus.olog.email;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmailNotifierTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
